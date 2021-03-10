6655338e959f7b4b3ed339a26cf42004636585b1
yassine_hammami <yassine_hammamii@yahoo.com>
2021-03-10 09:34:02 +0100

    change version

--- src/views/frontEnd/PageNotFound.vue
+++ src/views/frontEnd/PageNotFound.vue
@@ -14,7 +14,7 @@
                 <div class="bullshit__info">
                 </div>
                 <div class="bullshit__headline">{{ message }}</div>
-                <div class="bullshit__info">Please check that the URL you entered is correct, or click the button below to return to the homepage.</div>
+                <div class="bullshit__info">Please check that the URL you entered is correct</div>
                 <a @click="goToHome()" style="width:100%" class="bullshit__return-home">Back to home</a>
             </div>
         </div>
